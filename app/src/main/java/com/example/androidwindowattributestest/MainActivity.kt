5e7ae74cf842d0a16caa753f8ee1e99ace990d84
byoung-yun-park <byoung_yun.park@webtoonscorp.com>
2023-08-07 17:40:09 +0900

    windowFullScreen

--- app/src/main/java/com/example/androidwindowattributestest/MainActivity.kt
+++ app/src/main/java/com/example/androidwindowattributestest/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.androidwindowattributestest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val windowInsetsController =
+            WindowCompat.getInsetsController(window, window.decorView)
+        // Configure the behavior of the hidden system bars.
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
     }
 }
